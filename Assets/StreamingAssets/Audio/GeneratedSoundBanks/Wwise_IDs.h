5baec45261dcb488dc6797b9845b8195890f185c
Dbg1209 <dbg1209@gmail.com>
2023-10-22 13:25:07 -0500

    Implemented Wwise
    Implemented Wwise into the project

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,57 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID PLAY_WESTERNTRY = 1229291067U;
+        static const AkUniqueID STOP_WESTERNTRY = 1346157389U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace GAMEZONES
+        {
+            static const AkUniqueID GROUP = 2287985572U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GAMEPLAYZ1 = 120871162U;
+                static const AkUniqueID GAMEPLAYZ2 = 120871161U;
+                static const AkUniqueID GAMEPLAYZ3 = 120871160U;
+                static const AkUniqueID GAMEPLAYZ4 = 120871167U;
+                static const AkUniqueID MAINMENU = 3604647259U;
+            } // namespace SWITCH
+        } // namespace GAMEZONES
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
